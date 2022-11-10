c5c7f00025a6ff69d49773b0783f4393b273e817
choijin2022 <choeji2022@gmail.com>
2022-11-11 02:43:53 +0900

    순수가상함수 오버라이딩

--- lecture/Mammal.cpp
+++ lecture/Mammal.cpp
@@ -3,10 +3,13 @@
 using namespace std;
 
 class Mammal {
-protected:
+
 	int age;
 	string color;
 	string species;
+protected:
+	virtual void speak() = 0;
+	virtual void action() = 0;
 public:
 	Mammal() { set(); };
 	virtual void set(int age=0, string color="none", string species="mammal") {
@@ -14,44 +17,58 @@ public:
 		this->color = color;
 		this->species = species;
 	}
-	virtual void speak() { cout << "µ¿¹°ÀÌ ¿ï´Ù" << endl; }
-	virtual void action() { cout << species << "°¡ ¿òÁ÷ÀÌ´Ù" << endl; }
-	void show() { cout << age << "»ì µÈ " << color <<" " << species << "°¡ ¸»ÇÑ´Ù" << endl; }
+	string getSpecies() { return species; };
+	void show() { cout << age << "»ì µÈ " << color << " " << species << "°¡ ¸»ÇÑ´Ù" << endl; };
 
 };
 class Dog : public Mammal {
 public:
 	Dog(int age, string color) { set(age, color, "°³"); }
+	
 	void speak(){ cout << "¸Û¸Û Â¢´Ù!" << endl; }
-	void action(){ cout << species << "°¡ ²¿¸®¸¦ »ì¶û Èçµç´Ù" << endl; }
+	void action(){ cout << getSpecies() << "°¡ ²¿¸®¸¦ »ì¶û Èçµç´Ù" << endl; }
 };
 class Cat :public Mammal {
 public:
 	Cat(int age, string color) { set(age, color, "°í¾çÀÌ"); }
 	void speak() { cout << "¾ß¿Ë¾ß¿Ë ¿ï´Ù" << endl; }
-	void action() { cout << species << "°¡ ÇÞºµ ¾Æ·¡¼­ ³·ÀáÀ» ÀÚ´Ù" << endl; }
+	void action() { cout << getSpecies() << "°¡ ÇÞºµ ¾Æ·¡¼­ ³·ÀáÀ» ÀÚ´Ù" << endl; }
 };
 class Raccoon :public Mammal {
 public:
 	Raccoon(int age, string color) { set(age, color, "³Ê±¸¸®"); }
 	void speak() { cout << "³Ê±¸¸®°¡ ¿ï´Ù" << endl; }
-	void action() { cout << species << "°¡ ¸ÔÀÌ¸¦ ¾Ä´Â´Ù" << endl; }
+	void action() { cout << getSpecies() << "°¡ ¸ÔÀÌ¸¦ ¾Ä´Â´Ù" << endl; }
 };
 class Crow : public Mammal {
 public:
 	Crow(int age, string color) { set(age, color, "±î¸¶±Í"); }
 	void speak() { cout << "±î¾Ç±î¾Ç ¿ï´Ù" << endl; }
-	void action() { cout << species << "°¡ ÇÏ´ÃÀ» ³¯´Ù" << endl; }
+	void action() { cout << getSpecies() << "°¡ ÇÏ´ÃÀ» ³¯´Ù" << endl; }
 };
-int main() {
-	Dog temi(7,"Èò»ö");
-	Cat choonsik(3, "¾ó·è»ö");
-	Raccoon neoguri(5, "È¸»ö");
-	Crow crow(2, "°ËÀº»ö");
 
-	temi.show();
-	temi.action();
-	choonsik.show();
-	neoguri.show();
-	crow.show();
+
+int main() {
+	
+	Dog * temi = new Dog(7, "Èò»ö");
+	temi->show();
+	temi->action();
+	
+	Cat * choonsik = new Cat(3, "¾ó·è");
+	choonsik->show();
+	choonsik->action();
+
+	Raccoon* neoguri = new Raccoon(5, "È¸»ö");
+	neoguri->show();
+	neoguri->action();
+
+	Crow* crow = new Crow(2, "°ËÁ¤»ö");
+	crow->show();
+	crow->action();
+
+	delete temi;
+	delete choonsik;
+	delete neoguri;
+	delete crow;
+		 
 }
